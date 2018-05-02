f910a339b8add58f6548961004a2e68193c275bd
Felix Salazar <felix.salazar@augment.com>
2018-05-02 15:58:20 +0200

    new tests files.

---
+++ example/screen_Home.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import { YellowBox } from 'react-native';
+import screenAR from './screen_AR';
+
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, flexDirection: "column", alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Augment React Native Sample</Text>
+      <Button style={styles.button}
+          title="Open AR"
+          onPress={() => this.props.navigation.navigate('ARScreen', {
+              productToSearch: {
+                identifier: "1",
+                brand: "Rowenta",
+                name: "AIR Force Extreme",
+                ean: "3700342425321"
+              }
+          })} />
+      </View>
+    );
+  }
+}
+
+const RootStack = StackNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+    },
+    ARScreen: {
+        screen: screenAR,
+    }
+  },
+  {
+    initialRouteName: 'Home',
+  }
+);
+
+export default class App extends React.Component {
+  render() {
+    return <RootStack />;
+  }
+}
+
+const styles = StyleSheet.create({
+    buttonContainer: {
+        flex: 0,
+        margin: 8,
+        borderRadius: 5,
+        flexDirection: "row",
+        justifyContent: "space-around"
+    },
+    button: {
+        flex: 1,
+        height: 48,
+        backgroundColor: "skyblue"
+    }
+});
+
+YellowBox.ignoreWarnings([
+  'Warning: componentWillMount is deprecated',
+  'Warning: componentWillReceiveProps is deprecated',
+  'Warning: isMounted(...) is deprecated',
+  'Module'
+]);
