3b2a165ae750383c5cd25e4e4bdc7a75ad252c1a
Felix Salazar <felix.salazar@augment.com>
2018-05-30 16:01:40 +0200

    remove logs

--- ios/ReactAugment/ReactAugmentViewManager.m
+++ ios/ReactAugment/ReactAugmentViewManager.m
@@ -14,26 +14,20 @@ RCT_EXPORT_MODULE(AugmentReactPlayerNative)
 RCT_EXPORT_VIEW_PROPERTY(onPlayerReady, RCTDirectEventBlock)
 
 - (UIView*) view {
-    NSLog(@"GET newView");
     AugmentReactPlayerView* view = [AugmentReactPlayerView new];
     view.augmentReactPlayerViewDelegate = self;
     return view;
 }
 
 - (void)onInstantiationDone:(AugmentReactPlayerView *)augmentView {
-    NSLog(@"instat DONE");
     if (augmentView == NULL) {
       [ReactAugmentManager.augmentSDK.augmentPlayer unloadAll];
-        NSLog(@"instat DONE Pausing");
       return;
     }
 
-    //    augmentView.augmentPlayer = ReactAugmentManager.augmentSDK.augmentPlayer;
-    //    [augmentView.augmentPlayer resume];
     augmentView.augmentPlayer = ReactAugmentManager.augmentSDK.augmentPlayer;
     [ReactAugmentManager.augmentSDK.augmentPlayer resume];
     [self onPlayerReady:augmentView];
-    NSLog(@"instat DONE - exit");
 }
 
 - (void)onPlayerReady:(AugmentReactPlayerView *)augmentView {
