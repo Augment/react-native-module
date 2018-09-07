ad6333b299732eeb63858a3b3e9c7b58cb6a02b1
Stephane Garagnani <stephane@augment.com>
2018-09-07 16:53:03 +0200

    Remove android code

--- android/src/main/java/com/augment/reactplugin/AugmentReact.java
+++ android/src/main/java/com/augment/reactplugin/AugmentReact.java
@@ -7,7 +7,6 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
-import com.ar.augment.arplayer.*;
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.LifecycleEventListener;
 import com.facebook.react.bridge.Promise;
@@ -23,47 +22,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 @SuppressWarnings({"unused", "WeakerAccess"})
-public class AugmentReact extends ReactContextBaseJavaModule implements LifecycleEventListener, AugmentReactPlayerView.InstantiationCallback {
-
-    static final String REACT_CLASS = "AugmentReact";
-    static final String LOGTAG = AugmentReactPlayerView.class.getCanonicalName();
-
-    // Events names
-
-    static final String AUGMENT_EVENT_LOADING_PROGRESS = "augmentEventLoadingProgress";
-    static final String AUGMENT_EVENT_LOADING_OVER     = "augmentEventLoadingOver";
-
-    // Arguments keys
-
-    static final String ARG_APP_ID      = "id";
-    static final String ARG_APP_KEY     = "key";
-    static final String ARG_VUFORIA_KEY = "vuforia";
-    static final String ARG_IDENTIFIER  = "identifier";
-    static final String ARG_BRAND       = "brand";
-    static final String ARG_NAME        = "name";
-    static final String ARG_EAN         = "ean";
-
-    // Results keys
-
-    static final String ARG_ERROR        = "error";
-    static final String ARG_SUCCESS      = "success";
-    static final String ARG_MODEL_NUMBER = "model_number";
-
-    // Internals
-
-    AugmentReactPlayerView augmentReactPlayerView = null;
-    AugmentPlayerSDK augmentPlayerSDK;
-    Promise startPromise;
-    Promise productPromise;
+public class AugmentReact extends ReactContextBaseJavaModule implements LifecycleEventListener {
 
     public AugmentReact(ReactApplicationContext reactContext) {
         super(reactContext);
-
-        // Listen for lifecycle event, we need that for Augment SDK
-        reactContext.addLifecycleEventListener(this);
-
-        // Define a callback when a player will be instantiated
-        AugmentReactPlayerView.SetInstantiationCallback(this);
     }
 
     @Override
@@ -75,8 +37,6 @@ public class AugmentReact extends ReactContextBaseJavaModule implements Lifecycl
     @Override
     public Map<String, Object> getConstants() {
         HashMap<String, Object> constants = new HashMap<>();
-        constants.put("AUGMENT_EVENT_LOADING_PROGRESS", AUGMENT_EVENT_LOADING_PROGRESS);
-        constants.put("AUGMENT_EVENT_LOADING_OVER",     AUGMENT_EVENT_LOADING_OVER);
         return constants;
     }
 
@@ -86,12 +46,6 @@ public class AugmentReact extends ReactContextBaseJavaModule implements Lifecycl
      */
     @ReactMethod
     public void init(ReadableMap data) {
-        augmentPlayerSDK = new AugmentPlayerSDK(
-                getReactApplicationContext(),
-                data.getString(ARG_APP_ID),
-                data.getString(ARG_APP_KEY),
-                data.getString(ARG_VUFORIA_KEY)
-        );
     }
 
     /**
@@ -102,18 +56,7 @@ public class AugmentReact extends ReactContextBaseJavaModule implements Lifecycl
      */
     @ReactMethod
     public void checkIfModelDoesExistForUserProduct(final ReadableMap product, final Promise promise) {
-        ProductQuery query = BuildProductQueryFromProductHashMap(product);
-        augmentPlayerSDK.getProductDataController().checkIfModelDoesExistForUserProductQuery(query, new ProductDataController.ProductQueryListener() {
-            @Override
-            public void onResponse(@Nullable Product product) {
-                promise.resolve(getMapForProduct(product));
-            }
-
-            @Override
-            public void onError(WebserviceException e) {
-                promise.reject(e);
-            }
-        });
+        promise.reject(new Exception("Error: SDK is not available on Android."));
     }
 
     /**
@@ -123,24 +66,7 @@ public class AugmentReact extends ReactContextBaseJavaModule implements Lifecycl
      */
     @ReactMethod
     public void start(Promise promise) {
-        final Activity activity = getCurrentActivity();
-        if (activity == null) {
-            promise.reject(new Exception("Error: Activity is null."));
-            return;
-        }
-
-        startPromise = promise;
-    }
-
-    @Override
-    public void instantiationDone(AugmentReactPlayerView playerView) {
-        augmentReactPlayerView = playerView;
-        startARSession();
-    }
-
-    @Override
-    public void playerAttachedToWindow() {
-        startSuccess();
+        promise.reject(new Exception("Error: SDK is not available on Android."));
     }
 
     /**
@@ -151,17 +77,7 @@ public class AugmentReact extends ReactContextBaseJavaModule implements Lifecycl
      */
     @ReactMethod
     public void addProductToAugmentPlayer(ReadableMap product, Promise promise) {
-        if (augmentReactPlayerView == null) {
-            promise.reject(new Exception("addProductToAugmentPlayer() must be used after a success call to start()"));
-            return;
-        }
-
-        // Because adding a product to the ARView is a multi step operation
-        // We save the promise so we can access it latter when the operation
-        // has succeeded or failed
-        productPromise = promise;
-
-        getProduct(product);
+        promise.reject(new Exception("Error: SDK is not available on Android."));
     }
 
     /**
@@ -170,272 +86,12 @@ public class AugmentReact extends ReactContextBaseJavaModule implements Lifecycl
      */
     @ReactMethod
     public void recenterProducts(Promise promise) {
-        if (augmentReactPlayerView == null) {
-            promise.reject(new Exception("recenterProducts() must be used after a success call to start()"));
-            return;
-        }
-
-        if (augmentPlayerSDK == null) {
-            promise.reject(new Exception("Error: AugmentReactPlayerView is not present in this Activity."));
-            return;
-        }
-
-        augmentPlayerSDK.getAugmentPlayer().recenterProducts();
-        promise.resolve(getMapForSuccess(null));
+        promise.reject(new Exception("Error: SDK is not available on Android."));
     }
 
     @ReactMethod
     public void takeScreenshot(@NonNull final Promise promise){
-        augmentPlayerSDK.getAugmentPlayer().takeScreenshot(new ScreenshotTakerCallback() {
-            @Override
-            public void onScreenshotSaved(File file) {
-                promise.resolve(file.getAbsolutePath());
-            }
-
-            @Override
-            public void onError(Throwable throwable) {
-                promise.reject(throwable);
-            }
-        });
+        promise.reject(new Exception("Error: SDK is not available on Android."));
     }
 
-    // AR Logic
-
-    void startSuccess() {
-        if (startPromise != null) {
-            startPromise.resolve(getMapForSuccess(null));
-            startPromise = null;
-        }
-    }
-
-    void startError(String message) {
-        Log.d(LOGTAG, "Start Error: " + message);
-        if (startPromise != null) {
-            startPromise.reject(new Exception(message));
-            startPromise = null;
-        }
-    }
-
-    /**
-     * Init the AR view itself
-     */
-    void startARSession() {
-        final Activity activity = getCurrentActivity();
-        if (activity == null) {
-            startError("Activity is null.");
-            return;
-        }
-
-        if (augmentReactPlayerView == null) {
-            startError("AugmentReactPlayerView is not present in this Activity.");
-            return;
-        }
-
-        final AugmentPlayer augmentPlayer = augmentPlayerSDK.getAugmentPlayer();
-        augmentPlayer.initAR(activity, augmentReactPlayerView, new InitializationListener() {
-
-            @Override
-            public void onInitARDone(GLSurfaceView glSurfaceView, AugmentPlayerException e) {
-                // Check if there is no error
-                if (e != null) {
-                    sendEventLoadingOver();
-                    startError(e.getLocalizedMessage());
-                    return;
-                }
-
-                // For now it is the responsibility of the developer to start this asynchronously,
-                // this will evolve in the future
-                (new AsyncTask<Void, Void, Void>() {
-                    @Override
-                    protected Void doInBackground(Void... voids) {
-                        try {
-                            augmentPlayer.start();
-                            augmentPlayer.resume();
-                            startSuccess();
-                        } catch (final AugmentPlayerException e) {
-                            activity.runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    sendEventLoadingOver();
-                                    startError(e.getLocalizedMessage());
-                                }
-                            });
-                        }
-                        return null;
-                    }
-                }).execute();
-            }
-        });
-    }
-
-    void productSuccess() {
-        if (productPromise != null) {
-            productPromise.resolve(getMapForSuccess(null));
-            productPromise = null;
-        }
-    }
-
-    void productError(String message) {
-        Log.d(LOGTAG, "Product Error: " + message);
-        if (productPromise != null) {
-            productPromise.reject(new Exception(message));
-            productPromise = null;
-        }
-    }
-
-    void getProduct(ReadableMap product) {
-
-        ProductQuery query = AugmentReact.BuildProductQueryFromProductHashMap(product);
-        augmentPlayerSDK.getProductDataController().checkIfModelDoesExistForUserProductQuery(query, new ProductDataController.ProductQueryListener() {
-            @Override
-            public void onResponse(@Nullable com.ar.augment.arplayer.Product augmentProduct) {
-                if (augmentProduct == null) {
-                    sendEventLoadingOver();
-                    productError("This product is not available yet");
-                    return;
-                }
-
-                addToARView(augmentProduct);
-            }
-
-            @Override
-            public void onError(WebserviceException error) {
-                sendEventLoadingOver();
-                productError("This product is not available yet");
-            }
-        });
-    }
-
-    /**
-     * This is used to throttle the number of event sent to React native otherwise it become overloaded
-     */
-    private int lastProgress = 0;
-
-    void addToARView(@NonNull com.ar.augment.arplayer.Product product) {
-        augmentPlayerSDK.addModel3DToAugmentPlayerWithProduct(product, new LoaderCallback() {
-            @Override
-            public void onSuccess(String modelIdentifier) {
-                sendEventLoadingOver();
-                productSuccess();
-            }
-
-            @Override
-            public void onPreparingModel() {
-                lastProgress = 0;
-                sendEventLoadingProgress(0);
-            }
-
-            @Override
-            public void onError(WebserviceException error) {
-                sendEventLoadingOver();
-                productError(error.getLocalizedMessage());
-            }
-
-            @Override
-            public void onDownloadProgressUpdate(final Long aLong) {
-                int progress = Math.min(100, aLong.intValue());
-                if (progress <= lastProgress) return; // Throttling
-                lastProgress = progress;
-                sendEventLoadingProgress(progress);
-            }
-        });
-    }
-
-    // Activity lifecycle
-
-    @Override
-    public void onHostResume() {
-        if (augmentPlayerSDK != null) {
-            try {
-                augmentPlayerSDK.getAugmentPlayer().resume();
-            } catch (Exception e) {
-                Log.e(LOGTAG, "EXCEPTION: " + e.getLocalizedMessage(), e);
-            }
-        }
-    }
-
-    @Override
-    public void onHostPause() {
-        if (augmentPlayerSDK != null) {
-            try {
-                augmentPlayerSDK.getAugmentPlayer().pause();
-            } catch (Exception e) {
-                Log.e(LOGTAG, "EXCEPTION: " + e.getLocalizedMessage(), e);
-            }
-        }
-    }
-
-    @Override
-    public void onHostDestroy() {
-        if (augmentPlayerSDK != null) {
-            try {
-                augmentPlayerSDK.getAugmentPlayer().stop();
-            } catch (Exception e) {
-                Log.e(LOGTAG, "EXCEPTION: " + e.getLocalizedMessage(), e);
-            }
-        }
-    }
-
-    // Events
-
-    void sendEvent(String eventName, @Nullable WritableMap params) {
-        getReactApplicationContext()
-                .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
-                .emit(eventName, params);
-    }
-
-    protected void sendEventLoadingProgress(int value) {
-        WritableMap params = Arguments.createMap();
-        params.putInt("progress", value);
-        sendEvent(AUGMENT_EVENT_LOADING_PROGRESS, params);
-    }
-
-    protected void sendEventLoadingOver() {
-        sendEvent(AUGMENT_EVENT_LOADING_OVER, null);
-    }
-
-    // Helpers
-
-    protected WritableMap getMapForProduct(@Nullable Product product) {
-        WritableMap result = Arguments.createMap();
-        if (product == null) {
-            return result;
-        }
-
-        result.putString(ARG_IDENTIFIER,  product.getIdentifier());
-        result.putString(ARG_EAN,         product.getEan());
-        result.putString(ARG_BRAND,       product.getBrand());
-        result.putString(ARG_NAME,        product.getName());
-        result.putString(ARG_MODEL_NUMBER, product.getModelNumber());
-        return result;
-    }
-
-    protected WritableMap getMapForErrorMessage(String message) {
-        WritableMap result = Arguments.createMap();
-        result.putString(ARG_ERROR, message);
-        return result;
-    }
-
-    protected WritableMap getMapForSuccess(@Nullable String message) {
-        if (message == null) {
-            message = "ok";
-        }
-
-        WritableMap result = Arguments.createMap();
-        result.putString(ARG_SUCCESS, message);
-        return result;
-    }
-
-    public static ProductQuery BuildProductQueryFromProductHashMap(ReadableMap product) {
-        ProductQuery.Builder builder = new ProductQuery.Builder(
-                product.getString(ARG_IDENTIFIER),
-                product.getString(ARG_BRAND),
-                product.getString(ARG_NAME)
-        );
-
-        if (product.hasKey(ARG_EAN) && product.getString(ARG_EAN) != null && !product.getString(ARG_EAN).isEmpty()) {
-            builder.setEan(product.getString(ARG_EAN));
-        }
-        return builder.build();
-    }
 }
